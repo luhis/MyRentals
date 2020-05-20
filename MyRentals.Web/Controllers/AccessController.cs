6725b2ffd08f37d66d1e9778bd5581b2e81bb139
Matthew McCorry <mccorry@gmail.com>
2020-05-20 22:48:05 +0100

    refactor

--- MyRentals.Web/Controllers/AccessController.cs
+++ MyRentals.Web/Controllers/AccessController.cs
@@ -21,8 +21,8 @@
         public async Task<AccessModel> GetAccess()
         {
             var email = this.User.GetEmailAddress();
-            var realtorOrAdmin = await simpleRequirements.IsAdminOrRealtor(email);
-            var admin = simpleRequirements.IsAdmin(email);
+            var realtorOrAdmin = await this.simpleRequirements.IsAdminOrRealtor(email);
+            var admin = this.simpleRequirements.IsAdmin(email);
             return new AccessModel(true, realtorOrAdmin, admin);
         }
     }
